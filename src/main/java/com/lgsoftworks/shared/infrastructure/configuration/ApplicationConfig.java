3f9eed361b3d11dd5008dd7d72763d3392a1c0b8
Luis Alberto Gomez Arteaga <luisgomez24g@gmail.com>
2026-08-06 00:31:39 -0500

    refactor: Remove Redis dependency and related configuration

--- src/main/java/com/lgsoftworks/shared/infrastructure/configuration/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.lgsoftworks.shared.infrastructure.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-@Configuration
-@EnableRedisRepositories
-public class ApplicationConfig {
-}
